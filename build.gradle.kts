ec7421b9940888e42fc8edef5cad9b48b61e2f78
sonms <smsms5676@naver.com>
2025-08-29 02:02:34 +0900

    setting/#1 first commit - gradle, toml

---
+++ build.gradle.kts
@@ -0,0 +1,86 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.devtools.ksp)
+}
+
+val properties = Properties().apply {
+    load(project.rootProject.file("local.properties").inputStream())
+}
+
+android {
+    namespace = "com.example.navermaptest"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.navermaptest"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "BASE_URL", properties["base.url"].toString())
+        buildConfigField("String", "NAVERMAP_CLIENT_ID", properties.getProperty("NAVERMAP_CLIENT_ID"))
+        manifestPlaceholders["NAVERMAP_CLIENT_ID"] = properties.getProperty("NAVERMAP_CLIENT_ID")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.bundles.test)
+
+    debugImplementation(libs.bundles.debug)
+
+    implementation(libs.bundles.androidx)
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.kotlinx.immutable)
+
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.bundles.okhttp)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
+
+    implementation(libs.coil.compose)
+
+    implementation(libs.timber)
+
+    // 네이버
+    implementation(libs.bundles.naverMaps)
+
+}
